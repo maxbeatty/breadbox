d0eb47f308541a1af66c049c9a82ba01face030f
Max Beatty <max@anpret.com>
2011-07-24 20:13:57 -0700

    updated homepage

--- home.php
+++ home.php
@@ -3,11 +3,14 @@
 	get_sidebar();
 ?>
 
-<img src="<?php bloginfo('template_directory'); ?>/_/img/home_iphone.png" border="0" width="412px" height="276px" alt="our portfolio" id="iphone" usemap="iphonemap" />
 <map id="iphonemap" name="iphonemap">
 	<area shape="rect" coords="49,175,117,260" href="https://anpret.com/geolorean" alt="GeoLorean" title="GeoLorean"    />
 	<area shape="rect" coords="129,174,197,259" href="https://anpret.com/itappd" alt="iTappd" title="iTappd"    />
 </map>
+
+<img src="<?php bloginfo('template_directory'); ?>/_/img/home_iphone.png" border="0" width="412px" height="276px" alt="our portfolio" id="iphone" usemap="iphonemap" />
+
+<h2>We are a small team that devel­ops and designs web and mobile apps from start to finish.</h2>
 
 <?
 	get_footer(); 
